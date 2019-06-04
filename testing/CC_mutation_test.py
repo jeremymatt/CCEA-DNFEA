102e03e5d9261cc347020ad0988dd62ef7de7d38
Jeremy Matt <jmatt@uvm.edu>
2019-06-04 13:06:21 -0400

    messing around w/ cc mutation simulator

--- testing/CC_mutation_test.py
+++ testing/CC_mutation_test.py
@@ -10,22 +10,31 @@ import matplotlib.pyplot as plt
 
 
 L = 100
-Pw = .75
-num_reps=500
+Pw =1
+Pz = .25
+num_reps=1000
 num_runs = 50
+#
+#Pw_Pz_ratio = Pz/Pw
+#
+#predicted = L-L*(.5-.5*Pw_Pz_ratio)
+#
+#print('predicted: {}'.format(predicted))
+
 
 
 
 traces = []
 for i in range(num_runs):
-    clause = np.zeros(L)   
+    clause = np.ones(L)   
     trace = []
     for i in range(num_reps):
         
         for feature in range(L):
             if np.random.rand(1)[0] < 1/L:
                 if clause[feature]==0:
-                    clause[feature]=1
+                    if np.random.rand(1)[0] < Pz:
+                        clause[feature]=1
                 else:
                     if np.random.rand(1)[0] < Pw:
                         clause[feature]=0
@@ -38,6 +47,7 @@ yval = np.array(traces)
 yval = yval.mean(axis=0)
     
 plt.plot(range(num_reps),yval)
+plt.plot([0,num_reps],[predicted,predicted],'r--')
 plt.grid()
 plt.xlabel('Mutation Num')
 plt.ylabel('CC Order')
